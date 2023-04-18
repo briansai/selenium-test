f5065f7691109c2724b328975f63caa44c1d0225
Brian Sai <brianwsai@gmail.com>
2023-04-17 19:51:00 -0700

    Tested input field to add item

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,13 +1,16 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By, Key } = require('selenium-webdriver');
 
 async function example() {
   // launch the browser
   const driver = await new Builder().forBrowser('chrome').build();
 
   // navigate to our application
-  driver.get('https://lambdatest.github.io/sample-todo-app/');
+  await driver.get('https://lambdatest.github.io/sample-todo-app/');
+
   // add a todo
-  // close the browser
+  await driver
+    .findElement(By.id('sampletodotext'))
+    .sendKeys('Learn Selenium', Key.RETURN);
 }
 
 example();
