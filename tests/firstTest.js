02bdf6bafb09fbe573e6178343b8478ca4622a5f
Brian Sai <brianwsai@gmail.com>
2023-04-17 20:14:51 -0700

    Asserted item text to test validity of it

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,4 +1,5 @@
 const { Builder, By, Key } = require('selenium-webdriver');
+const assert = require('assert');
 
 async function example() {
   // launch the browser
@@ -11,6 +12,18 @@ async function example() {
   await driver
     .findElement(By.id('sampletodotext'))
     .sendKeys('Learn Selenium', Key.RETURN);
+
+  // assert
+  let todoText = await driver
+    .findElement(By.xpath('//li[last()]'))
+    .getText()
+    .then((val) => {
+      return val;
+    });
+
+  assert.strictEqual(todoText, 'Learn Selenium]');
+
+  console.log(todoText);
 }
 
 example();
