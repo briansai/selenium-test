b88a588270d561d64f9e716bd55ee351fc108adc
Brian Sai <brianwsai@gmail.com>
2023-04-20 17:11:55 -0700

    Implemented Mocha and refactored code

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,34 +1,32 @@
 const { Builder, By, Key } = require('selenium-webdriver');
-const assert = require('assert');
-const should = require('chai').should();
+// const assert = require('assert');
+// const should = require('chai').should();
 
-async function example() {
-  // launch the browser
-  const driver = await new Builder().forBrowser('chrome').build();
+desecribe('add todo tests', () => {
+  it('successfully adds a todo item to application', async () => {
+    // launch the browser
+    const driver = await new Builder().forBrowser('chrome').build();
 
-  // navigate to our application
-  await driver.get('https://lambdatest.github.io/sample-todo-app/');
+    // navigate to our application
+    await driver.get('https://lambdatest.github.io/sample-todo-app/');
 
-  // add a todo
-  await driver
-    .findElement(By.id('sampletodotext'))
-    .sendKeys('Learn Selenium', Key.RETURN);
+    // add a todo
+    await driver
+      .findElement(By.id('sampletodotext'))
+      .sendKeys('Learn Selenium', Key.RETURN);
 
-  // assert
-  let todoText = await driver
-    .findElement(By.xpath('//li[last()]'))
-    .getText()
-    .then((val) => {
-      return val;
-    });
+    // assert
+    let todoText = await driver
+      .findElement(By.xpath('//li[last()]'))
+      .getText()
+      .then((val) => {
+        return val;
+      });
 
-  // assert using node assertion
-  assert.strictEqual(todoText, 'Learn Selenium');
+    // assert using node assertion
+    // assert.strictEqual(todoText, 'Learn Selenium');
 
-  // assert using chai should
-  todoText.should.equal('Learn Seleniu]m');
-
-  console.log(todoText);
-}
-
-example();
+    // assert using chai should
+    // todoText.should.equal('Learn Selenium');
+  });
+});
